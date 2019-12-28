7f56a6c85655a68b8da79ebe474eaa05b6e83dc9
dylink <dylan2a@hotmail.fr>
2019-12-28 17:58:11 +0100

    - Adding class files

---
+++ hello-node-master/apps/base/js/classes.js
@@ -0,0 +1,29 @@
+class Ball{
+
+  constructor (x, y, ball_number, in_out, ball_color){
+    this.x = x;
+    this.y = y;
+    this.ball_number = ball_number;
+    this.in_out = in_out;
+    this.ball_color = ball_color;
+  }
+
+  get positionX(){ return this.x;}
+  get positionY(){ return this.y;}
+  get ball_number(){ return this.ball_number;}
+  get in_out(){ return this.in_out;}
+  get ball_color(){ return this.ball_color;}
+
+}
+
+class Stick{
+
+  constructor (force, angle){
+    this.force = force;
+    this.angle = angle;
+  }
+
+  get force(){ return this.force;}
+  get angle(){ return this.angle;}
+
+}
