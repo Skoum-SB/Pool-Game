dd1f4155bd5c85bbbe6bed8add27c320f3b70885
dylink <dylan2a@hotmail.fr>
2020-01-05 18:35:07 +0100

    - Trying to add collision

--- hello-node-master/apps/base/js/classes.js
+++ hello-node-master/apps/base/js/classes.js
@@ -6,9 +6,11 @@ class Ball{
     this.color = color;
     this.image = document.createElement("img");
     this.image.onload;
+    this.ismoving = false;
     this.vitessex;
     this.vitessey;
     this.area = area;
+    this.radius = 19;
   }
 
   draw(){
@@ -18,13 +20,34 @@ class Ball{
     }
   }
 
-  move(x, y, slow){
+  collideWith(second_ball, allBalls){
+      let dx = second_ball.x - this.x;
+      let dy = second_ball.y - this.y;
+      let distance = Math.sqrt(dx * dx + dy * dy);
+      if(distance  == 0){
+        console.log(distance)
+      }
+
+      if(this != second_ball && distance < this.radius){
+        //faire les caculs
+        if(!second_ball.ismoving){
+          console.log("Aiiiiie");
+             //second_ball.move(allBalls);
+        }
+      }
+  }
+
+  move(x, y, slow, allBalls){
+    this.ismoving = true;
     let slowx = (x>y) ? slow : slow*(x/y);
     let slowy = (x<y) ? slow : slow*(y/x);
     this.moveit = () => {
       if((Math.abs(y)-slowy > 0) || (Math.abs(x)-slowx > 0)){
         /*Left and Right*/ if(this.x < 55+25 || this.x > 1395+25){ x = -x;}
         /*Top and Bottom*/ if(this.y < 55+25 || this.y > 717+25){ y = -y;}
+        for(var i=0; i<allBalls.length; i++){
+            this.collideWith(allBalls[i],allBalls);
+          }
         this.x += x;
         this.y += y;
         x += (x > 0) ? -slowx : slowx;
@@ -33,6 +56,7 @@ class Ball{
       }
       else{
         cancelAnimationFrame(this.moveit);
+        this.ismoving = false;
       }
     }
     this.moveit();
