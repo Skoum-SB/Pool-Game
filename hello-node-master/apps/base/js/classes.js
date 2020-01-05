b828c1a62a1f735f28a8e8c7c9e20421c39e06a3
VeroniqueHuang <veroniquehuang@hotmail.fr>
2020-01-05 22:54:47 +0100

    changing formula of collision

--- hello-node-master/apps/base/js/classes.js
+++ hello-node-master/apps/base/js/classes.js
@@ -35,11 +35,12 @@ class Ball{
         /*Left and Right*/ if(this.x < 55+25 || this.x > 1395+25){ this.vx = -this.vx;}
         /*Top and Bottom*/ if(this.y < 55+25 || this.y > 717+25){ this.vy = -this.vy;}
 
+        /*
         if(this.vx>0){this.vx -=0.02;}
         if(this.vy>0){this.vy -=0.02;}
 
         if(this.vx<0){this.vx +=0.02;}
-        if(this.vy<0){this.vy +=0.02;}
+        if(this.vy<0){this.vy +=0.02;} */
 
 
         //this.vx *= 0.98;
@@ -64,8 +65,8 @@ class Ball{
     var distance = Math.sqrt(dx * dx + dy * dy);
 
     if(this!=second_ball && (distance < this.radius*2) ){
-      
-        if(second_ball.vx>0){second_ball.vx -=0.03;}
+
+      /*  if(second_ball.vx>0){second_ball.vx -=0.03;}
         if(second_ball.vy>0){second_ball.vy -=0.03;}
 
         if(second_ball.vx<0){second_ball.vx +=0.03;}
@@ -75,7 +76,7 @@ class Ball{
         if(this.vy>0){this.vy -=0.01;}
 
         if(this.vx<0){this.vx +=0.01;}
-        if(this.vy<0){this.vy +=0.01;}
+        if(this.vy<0){this.vy +=0.01;}*/
 
       //this.vx = -this.vx;
       //this.vx = -this.vx;
@@ -83,25 +84,46 @@ class Ball{
         second_ball.move(allBalls);
       }
 
-      var angle = Math.atan2(dy,dx);
-      var sin = Math.sin(angle);
-      var cos = Math.cos(angle);
-      var x1=0; var y1=0;
+      let angle = Math.atan2(dy,dx);
+      let sin = Math.sin(angle);
+      let cos = Math.cos(angle);
+      let x1=0; let y1=0;
+      let x2 = dx*cos+dy*sin;
+      let y2 = dy*cos-dx*sin;
 
       //Rotate velocity
-      var vx1 = this.vx*cos+this.vy*sin;
-      var vy1 = this.vy*cos-this.vx*sin;
-      var vx2 = second_ball.vx*cos+second_ball.vy*sin;
-      var vy2 = second_ball.vy*cos-second_ball.vx*sin;
-
+      let vx1 = this.vx*cos+this.vy*sin;
+      let vy1 = this.vy*cos-this.vx*sin;
+      let vx2 = second_ball.vx*cos+second_ball.vy*sin;
+      let vy2 = second_ball.vy*cos-second_ball.vx*sin;
 
       //resolve 1D velocity, use temp variables
-      var vx1final = ((this.mass-second_ball.mass)*vx1+2*second_ball.mass*vx2)/(this.mass+second_ball.mass);
-      var vx2final = ((second_ball.mass-this.mass)*vx2+2*this.mass*vx1)/(this.mass+second_ball.mass);
+      let vx1final = ((this.mass-second_ball.mass)*vx1+2*second_ball.mass*vx2)/(this.mass+second_ball.mass);
+      let vx2final = ((second_ball.mass-this.mass)*vx2+2*this.mass*vx1)/(this.mass+second_ball.mass);
       // update velocity
       vx1 = vx1final;
       vx2 = vx2final;
 
+      // fix the glitch by moving ball part equal to the overlap
+      let absV = Math.abs(vx1)+Math.abs(vx2);
+      let overlap = (this.radius+second_ball.radius)-Math.abs(x1-x2);
+      x1 += vx1/absV*overlap;
+      x2 += vx2/absV*overlap;
+
+      // rotate the relative positions back
+      let x1final = x1*cos-y1*sin;
+      let y1final = y1*cos+x1*sin;
+      let x2final = x2*cos-y2*sin;
+      let y2final = y2*cos+x2*sin;
+
+      // finally compute the new absolute positions
+       second_ball.x = this.x + x2final;
+       second_ball.y = this.y + y2final;
+
+       this.x = this.x + x1final;
+       this.y = this.y + y1final;
+
+       //rotate vel back
       this.vx = vx1*cos-vy1*sin;
       this.vy = vy1*cos+vx1*sin;
       second_ball.vx = vx2*cos-vy2*sin;
