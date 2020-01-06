2feb0ca4d9574fe193be334d116100a9b504800a
Simon Bensiam <56318891+Skoum-SB@users.noreply.github.com>
2020-01-06 01:46:39 +0100

    BIG UPDATE

--- hello-node-master/apps/base/_server/index.js
+++ hello-node-master/apps/base/_server/index.js
@@ -4,6 +4,9 @@ class Base extends ModuleBase {
 
 	constructor(app, settings) {
 		super(app, new Map([["name", "baseapp"], ["io", true]]));
+		this.lobbyPlayers = [];
+		this.gamePlayers = [[{id: null, name : null},{id: null, name : null}]];
+		this.gameRooms = [];
 	}
 
 	/**
@@ -12,25 +15,25 @@ class Base extends ModuleBase {
 	 * @param {*} res 
 	 * @param  {...*} params : some arguments
 	 */
-	hello(req, res, ... params) {
+	/*hello(req, res, ... params) {
 		let answer = ["hello", ...params, "!"].join(" "); // say hello
 		trace(answer); // say it
 		this.sendJSON(req, res, 200, {message: answer}); // answer JSON
-	}
+	}*/
 
 	/**
 	 * @method data : random data response
 	 * @param {*} req 
 	 * @param {*} res 
 	 */
-	data(req, res) {
+	/*data(req, res) {
 		let data = [ // some random data
 			{id: 0, name: "data0", value: Math.random()},
 			{id: 1, name: "data1", value: Math.random()},
 			{id: 2, name: "data2", value: Math.random()}
 		];
 		this.sendJSON(req, res, 200, data); // answer JSON
-	}
+	}*/
 
 	/**
 	 * @method _onIOConnect : new IO client connected
@@ -38,14 +41,126 @@ class Base extends ModuleBase {
 	 */
 	_onIOConnect(socket) {
 		super._onIOConnect(socket); // do not remove super call
-		socket.on("dummy", packet => this._onDummyData(socket, packet)); // listen to "dummy" messages
+		//socket.on("dummy", packet => this._onDummyData(socket, packet)); // listen to "dummy" messages
+		socket.on("login", packet => this._onLogin(socket, packet));
 	}
 
-	_onDummyData(socket, packet) { // dummy message received
+	/*_onDummyData(socket, packet) { // dummy message received
 		trace(socket.id, "dummy", packet); // say it
 		socket.emit("dummy", {message: "dummy indeed", value: Math.random()}); // answer dummy random message
+	}*/
+
+	_onLogin(socket, packet){
+		trace(socket.id, "pseudo :", packet);
+		
+		socket.emit("players", this.lobbyPlayers);
+
+		this.lobbyPlayers.push({id: socket.id, name: packet});
+
+		trace(this.lobbyPlayers.length, " players connected actually");
+
+		socket.broadcast.emit("newplayer", this.lobbyPlayers[this.lobbyPlayers.length - 1]);
+
+		socket.on("disconnect", packet => this._onDisconnect(socket, packet));
+		socket.on("challenge", packet => this._onChallenge(socket, packet));
 	}
 
+	_onDisconnect(socket, packet){
+		trace(socket.id, "disconnected");
+
+		var inLobby = false;
+		for(var i = 0; i < this.lobbyPlayers.length; i++){
+			if(this.lobbyPlayers[i].id == socket.id){
+				var playerDisconnected = this.lobbyPlayers.splice(i, 1);
+				inLobby = true;
+			}
+		}
+		
+		if(inLobby){//In lobby
+			trace(this.lobbyPlayers.length, " players in the lobby actually");
+			socket.broadcast.emit("playerDisconnected", playerDisconnected[0].name);
+		}
+
+		else{//In game
+			for(var i=0 ; i<this.gamePlayers.length ; i++){
+				if(socket.id == this.gamePlayers[i][0].id){
+					trace("DISCONNECT P1", "room-" + i);
+					this._io.emit("newplayer", this.gamePlayers[i][1]);
+					socket.to("room-" + i).emit("playerLeft", this.lobbyPlayers);
+					this.lobbyPlayers.push({id: this.gamePlayers[i][1].id, name: this.gamePlayers[i][1].name});
+					this.gameRooms[i] = false;
+					this.gamePlayers[i] = [{id: null, name : null},{id: null, name : null}];
+
+				}
+				else if(socket.id == this.gamePlayers[i][1].id){
+					trace("DISCONNECT P2", "room-" + i);
+					this._io.emit("newplayer", this.gamePlayers[i][0]);
+					socket.to("room-" + i).emit("playerLeft", this.lobbyPlayers);
+					this.lobbyPlayers.push({id: this.gamePlayers[i][0].id, name: this.gamePlayers[i][0].name});
+					this.gameRooms[i] = false;
+					this.gamePlayers[i] = [{id: null, name : null},{id: null, name : null}];
+
+				}
+			}
+
+
+		}
+	}
+
+	_onChallenge(socket, packet){//packet = Name Player 2
+		trace(socket.id, packet);//socket.id = Id Player 1
+		
+		var playerName;//Name Player 1
+		var opponentId;//Id Player 2
+
+		var roomId = 0;
+
+		for(var i=0 ; i<this.gameRooms.length ; roomId = ++i){
+			trace("ITERATION", i);
+			if(this.gamePlayers[i][0].id == null){
+				break;
+			}
+		}
+		this.gameRooms[roomId] = true;
+		trace(this.gameRooms.length, "ID GAME = ", roomId);
+
+		//Prepare next room (If necessary)
+		if(roomId == (this.gamePlayers.length - 1)){
+			trace("INCREASED SIZE");
+			this.gamePlayers.push([{id: null, name : null},{id: null, name : null}]);
+		}
+
+		this.lobbyPlayers.forEach(el => {
+			if(el.name == packet)
+				opponentId = el.id;//Id Player 2
+			if(el.id == socket.id)
+				playerName = el.name;//Name Player 1
+		});
+		trace(opponentId);
+
+		socket.join("room-" + (roomId));
+
+		this._io.sockets[opponentId].join("room-" + (roomId));
+		this._io.to("room-" + (roomId)).emit("start", roomId);
+
+		//trace(this._io);
+
+		var gamePlayers = [playerName, packet];//Name of the 2 players
+		trace(gamePlayers);
+		trace(this.gameRooms);
+
+		socket.broadcast.emit("playersInGame", gamePlayers);
+
+		this.gamePlayers[roomId] = [{id: socket.id, name: playerName}, {id: opponentId, name: packet}];
+		trace(roomId, this.gamePlayers[roomId]);
+
+		for(var i = 0; i < this.lobbyPlayers.length; i++){
+			if(this.lobbyPlayers[i].name == gamePlayers[0] || this.lobbyPlayers[i].name == gamePlayers[1]){
+				this.lobbyPlayers.splice(i, 1);
+				i--;
+			}
+		}
+	}
 }
 
 module.exports = Base; // export app class
--- hello-node-master/apps/base/js/main.js
+++ hello-node-master/apps/base/js/main.js
@@ -35,18 +35,45 @@ class Base {
 	 * @method onIOConnect : socket is connected
 	 */
 	onIOConnect() {
-		trace("yay IO connected");
-		this.io.on("dummy", packet => this.onDummyData(packet)); // listen to "dummy" messages
-		this.io.emit("dummy", {value: "dummy data from client"}) // send test message
+		this.io.on("players", packet => this.onLogin(packet));
 	}
 
-	/**
-	 * @method onDummyData : dummy data received from io server
-	 * @param {Object} data
-	 */
-	onDummyData(data) {
-		trace("IO data", data);
-		this.mvc.controller.ioDummy(data); // send it to controller
+	onLogin(data){
+		this.mvc.controller.getPlayers(data);
+		this.io.on("newplayer", packet => this.onNewPlayer(packet));
+		this.io.on("playerDisconnected", packet => this.onDisconnectedPlayer(packet));
+		this.io.on("start", packet => this.onStart(packet));
+		this.io.on("playersInGame", packet => this.onPlayersInGame(packet));
+	}
+
+	onNewPlayer(data){
+		this.mvc.controller.getNewPlayer(data);
+		trace("Newcomer !!!");
+	}
+
+	onDisconnectedPlayer(data){
+		this.mvc.controller.getDCPlayer(data);
+		trace("DC", data);
+	}
+
+	onStart(room){
+		trace("Starting the game", room);
+		this.mvc.controller.start(room); // send it to controller
+		this.io.removeAllListeners();
+		this.io.on("playerLeft", packet => this.onplayerLeft(packet));
+	}
+
+	onPlayersInGame(players){
+		this.mvc.controller.playersInGame(players);
+	}
+
+	onplayerLeft(players){
+		trace("Disconnected player");
+		this.mvc.controller.playerLeftGame(players);
+		this.io.on("newplayer", packet => this.onNewPlayer(packet));
+		this.io.on("playerDisconnected", packet => this.onDisconnectedPlayer(packet));
+		this.io.on("start", packet => this.onStart(packet));
+		this.io.on("playersInGame", packet => this.onPlayersInGame(packet));
 	}
 }
 
@@ -59,13 +86,93 @@ class MyModel extends Model {
 	async initialize(mvc) {
 		super.initialize(mvc);
 
+		this.name = "";
+		this.players = [];
+		this.room = 0;
+
+		this.area = new Area();
+		this.image = document.createElement("img");
+		this.image.src = 'images/sprbackground4.png';
+
+		this.redballs = [
+		new Ball(this.area, 1056,433,"red"),
+		new Ball(this.area, 1090,374,"red"),
+		new Ball(this.area, 1126,393,"red"),
+		new Ball(this.area, 1126,472,"red"),
+		new Ball(this.area, 1162,335,"red"),
+		new Ball(this.area, 1162,374,"red"),
+		new Ball(this.area, 1162,452,"red")
+		];
+
+		this.yellowballs = [
+		new Ball(this.area, 1022,413,"yellow"),
+		new Ball(this.area, 1056,393,"yellow"),
+		new Ball(this.area, 1090,452,"yellow"),
+		new Ball(this.area, 1126,354,"yellow"),
+		new Ball(this.area, 1126,433,"yellow"),
+		new Ball(this.area, 1162,413,"yellow"),
+		new Ball(this.area, 1162,491,"yellow")
+		];
+
+		this.whiteball = new Ball(this.area, 413,413,"white");
+		this.blackball = new Ball(this.area, 1090,413,"black");
+
+		this.balls = [
+			this.yellowballs[0],
+			this.yellowballs[1],
+			this.yellowballs[2],
+			this.yellowballs[3],
+			this.yellowballs[4],
+			this.yellowballs[5],
+			this.yellowballs[6],
+			this.redballs[0],
+			this.redballs[1],
+			this.redballs[2],
+			this.redballs[3],
+			this.redballs[4],
+			this.redballs[5],
+			this.redballs[6],
+			this.blackball,
+			this.whiteball
+		];
+
+
 	}
 
-	async data() {
+	/*async data() {
 		trace("get data");
 		// keep data in class variable ? refresh rate ?
 		let result = await Comm.get("data"); // wait data from server
 		return result.response; // return it to controller
+	}*/
+
+	login(name){
+		this.name = name;
+	}
+
+	addOtherPlayers(otherPlayers){
+		var names = [];
+		otherPlayers.forEach(player => {
+			names.push((player.name));
+		});
+		this.players = names;
+	}
+
+	addNewPlayer(newPlayer){
+		this.players.push(newPlayer.name);
+	}
+
+	removePlayer(playerName){
+		trace(playerName);
+		for(var i = 0; i < this.players.length; i++){
+			if(this.players[i] == playerName){
+				this.players.splice(i, 1);
+			}
+		}
+	}
+
+	changeRoom(room){
+		this.room = room;
 	}
 
 }
@@ -74,13 +181,12 @@ class MyView extends View {
 
 	constructor() {
 		super();
-		this.table = null;
 	}
 
 	initialize(mvc) {
 		super.initialize(mvc);
 
-		// create get test btn
+		/*// create get test btn
 		this.btn = document.createElement("button");
 		this.btn.innerHTML = "get test";
 		this.stage.appendChild(this.btn);
@@ -88,17 +194,23 @@ class MyView extends View {
 		// create io test btn
 		this.iobtn = document.createElement("button");
 		this.iobtn.innerHTML = "io test";
-		this.stage.appendChild(this.iobtn);
+		this.stage.appendChild(this.iobtn);*/
 
-		// io random value display
-		this.iovalue = document.createElement("div");
-		this.iovalue.innerHTML = "no value";
-		this.stage.appendChild(this.iovalue);
+		var text = document.createTextNode('Votre Pseudo :');
+		this.stage.appendChild(text);
+		this.stage.appendChild(document.createElement("br"));
 
-		// get dataset display
-		this.table = document.createElement("table");
-		this.stage.appendChild(this.table);
+		this.nameInput = document.createElement("input");
+		this.nameInput.id = "name";
+		this.nameInput.setAttribute('type',"text");
+		this.stage.appendChild(this.nameInput);
 
+		this.submitInput = document.createElement("button");
+		this.submitInput.innerHTML = "Valider";
+
+		this.stage.appendChild(this.submitInput);
+
+		//this.stage.appendChild(this.mvc.model.area.cvs);
 	}
 
 
@@ -115,43 +227,128 @@ class MyView extends View {
 	}
 
 	addListeners() {
-		this.getBtnHandler = e => this.btnClick(e);
-		this.btn.addEventListener("click", this.getBtnHandler);
+		this.submitHandler = e => this.submitName(e);
+		this.submitInput.addEventListener("click", this.submitHandler);
 
-		this.ioBtnHandler = e => this.ioBtnClick(e);
-		this.iobtn.addEventListener("click", this.ioBtnHandler);
+		this.unloadHandler = e => this.unload(e);
+		this.stage.addEventListener("unload", this.unloadHandler);
 	}
 
 	removeListeners() {
-		this.btn.removeEventListener("click", this.getBtnHandler);
-		this.iobtn.removeEventListener("click", this.ioBtnHandler);
+		/*this.btn.removeEventListener("click", this.getBtnHandler);
+		this.iobtn.removeEventListener("click", this.ioBtnHandler);*/
 	}
 
-	btnClick(event) {
-		this.mvc.controller.btnWasClicked("more parameters"); // dispatch
+	submitName(event){
+		this.mvc.controller.submitName(document.getElementById("name").value);
 	}
 
-	ioBtnClick(event) {
-		this.mvc.controller.ioBtnWasClicked("io parameters"); // dispatch
+	unload(event){
+		this.mvc.controller.disconnect();
 	}
 
-	update(data) {
+	update(){
 		while(this.table.firstChild) this.table.removeChild(this.table.firstChild); // empty table
-		data.forEach(el => { // loop data
+		var data = this.mvc.model.players;
+		trace(data);
+
+		document.getElementById("ownName").innerHTML = "Vous : " + this.mvc.model.name;
+
+		for(var i = 0 ; i < data.length ; i++){
 			let line = document.createElement("tr"); // create line
-			Object.keys(el).forEach(key => { // loop object keys
-				let cell = document.createElement("td"); // create cell
-				cell.innerHTML = el[key]; // display
-				line.appendChild(cell); // add cell
-			});
+			let cell = document.createElement("td"); // create cell
+			cell.innerHTML = data[i];
+			line.appendChild(cell); // add cell
+
+			cell = document.createElement("td"); //second cell
+			let btn = document.createElement("button");
+			btn.innerHTML = "Défier";
+			btn.id = i;
+			btn.onclick = () => {this.mvc.controller.challenge(btn.id)};
+			cell.appendChild(btn);
+			line.appendChild(cell);
+
 			this.table.appendChild(line); // add line
-		});
+		}
 	}
 
-	updateIO(value) {
-		this.iovalue.innerHTML = value.toString(); // update io display
+	lobby(){
+		this.stage.innerHTML = "";
+		this.stage.appendChild(document.createTextNode("Joueurs actuellement disponibles :"));
+		this.stage.appendChild(document.createElement("br"));
+		this.stage.appendChild(document.createElement("br"));
+
+		var ownName = document.createElement("b");
+		ownName.id = "ownName";
+		this.stage.appendChild(ownName);
+
+		this.stage.appendChild(document.createElement("br"));
+		this.stage.appendChild(document.createElement("br"));
+
+		this.stage.appendChild(document.createTextNode("Autres joueurs :"));
+
+		this.stage.appendChild(document.createElement("br"));
+		this.stage.appendChild(document.createElement("br"));
+
+		this.table = document.createElement("table");
+		this.stage.appendChild(this.table);
 	}
 
+	/*waitAnswer(){
+		this.stage.innerHTML = "";
+		this.stage.appendChild(document.createTextNode("En attente de la réponse de votre adversaire"));
+	}*/
+
+	startGame(){
+		/*this.stage.innerHTML = "";
+		this.stage.appendChild(document.createTextNode("Vous êtes dans la Room N°" + (this.mvc.model.room + 1)));
+		this.cvs = document.createElement("canvas");
+		this.ctx = this.cvs.getContext("2d");
+		this.img = document.createElement("img");
+		this.img.src = 'images/sprbackground4.png';
+
+		this.img.onload = () => {
+			this.imageRatio = window.innerHeight/this.img.naturalHeight;
+			this.ratio = window.innerWidth/this.img.naturalWidth;
+			this.cvs.width = window.innerWidth;
+			this.cvs.height = window.innerHeight;
+			if(this.img.naturalHeight*this.ratio > window.innerHeight-40){
+				console.log("Oui");
+				this.width = this.img.naturalWidth*this.imageRatio-40;
+				this.height = this.img.naturalHeight*this.imageRatio-40;
+			}
+			else{
+				this.width = this.img.naturalWidth*this.ratio;
+				this.height = this.img.naturalHeight*this.ratio;
+			}
+			this.ctx.drawImage(this.img, 0, 0, this.img.naturalWidth, this.img.naturalHeight, 0, 0, this.width, this.height);
+		}
+
+		window.onresize = () => {
+			this.img.onload();
+		};
+
+		this.stage.appendChild(this.cvs);
+
+		var canvas = document.createElement("canvas");
+		var ctex = canvas.getContext("2d");
+		var image = document.createElement("yellowball.png");
+		*/
+		this.stage.style.backgroundColor = "black";
+		this.stage.innerHTML = "";
+		this.stage.appendChild(this.mvc.model.area.cvs);
+
+		this.display = () => {
+			this.mvc.model.image.onload;
+			trace("yep");
+			this.mvc.model.area.clear();
+			this.mvc.model.area.draw(this.mvc.model.image);
+			for(let i = 0; i < this.mvc.model.balls.length; i++)
+				this.mvc.model.balls[i].draw();
+			requestAnimationFrame(this.display);
+		}
+		this.display();
+	}
 }
 
 class MyController extends Controller {
@@ -165,18 +362,62 @@ class MyController extends Controller {
 
 	}
 
-	async btnWasClicked(params) {
+	/*async btnWasClicked(params) {
 		trace("btn click", params);
-		this.mvc.view.update(await this.mvc.model.data()); // wait async request > response from server and update view table values
+		//this.mvc.view.update(await this.mvc.model.data()); // wait async request > response from server and update view table values
 	}
 
 	async ioBtnWasClicked(params) {
 		trace("io btn click", params);
 		this.mvc.app.io.emit("dummy", {message: "dummy io click"}); // send socket.io packet
+	}*/
+
+	submitName(name){
+		trace("submit btn click", name);
+		this.mvc.model.login(name);
+		this.mvc.app.io.emit("login", name);
+		this.mvc.view.lobby();
 	}
 
-	ioDummy(data) {
-		this.mvc.view.updateIO(data.value); // io dummy data received from main app
+	disconnect(){
+		this.mvc.app.io.emit("disconnect");
 	}
 
+	getPlayers(players){
+		this.mvc.model.addOtherPlayers(players);
+		this.mvc.view.update();
+	}
+
+	getNewPlayer(player){
+		this.mvc.model.addNewPlayer(player);
+		this.mvc.view.update();
+	}
+
+	getDCPlayer(playerName){
+		this.mvc.model.removePlayer(playerName);
+		this.mvc.view.update();
+	}
+
+	playersInGame(players){
+		this.mvc.model.removePlayer(players[0]);
+		this.mvc.model.removePlayer(players[1]);
+		this.mvc.view.update();
+	}
+
+	start(room){
+		this.mvc.model.changeRoom(room);
+		this.mvc.view.startGame();
+		//this.mvc.view.display();
+	}
+
+	challenge(opponent){
+		trace(opponent);
+		this.mvc.app.io.emit("challenge", this.mvc.model.players[opponent]);
+		//this.mvc.view.waitAnswer();
+	}
+
+	playerLeftGame(players){
+		this.mvc.view.lobby();
+		this.getPlayers(players);
+	}
 }
